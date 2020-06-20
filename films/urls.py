4ae704fd571f96d8456708447bd824f04e47f23a
mixxamm <maximjanssensj5@gmail.com>
2020-06-20 17:45:37 +0200

    Voeg zoekbalk toe & ook overzicht

--- films/urls.py
+++ films/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path('', views.index, name='films'),]
+urlpatterns = [path('', views.index, name='films'), path('search/', views.search, name='search')]
--- films/views.py
+++ films/views.py
@@ -1,3 +1,4 @@
+from django.core.serializers import json
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -5,4 +6,15 @@ from django.http import HttpResponse
 def index(request):
     file = open(r'films\movies.txt')
     movies = file.readlines()
-    return render(request, 'films/index.html', {'movies': movies})
+    if request.method == 'GET':
+        return render(request, 'films/index.html', {'movies': movies})
+    elif request.method == 'POST':
+        search = request.POST["search"]
+        results = []
+        for movie in movies:
+            if(search in movie):
+                results.append(movie)
+        return HttpResponse(results)
+
+def search(request):
+    return render(request, 'films/postMovie.html')
