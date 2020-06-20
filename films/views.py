724942396a809f223f42ad808107de804d38af97
mixxamm <maximjanssensj5@gmail.com>
2020-06-20 18:04:34 +0200

    Voeg redis functionaliteit toe

--- films/views.py
+++ films/views.py
@@ -1,6 +1,13 @@
 from django.core.serializers import json
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import dateformat
+import datetime
+
+import redis
+
+
+redisClient = redis.Redis(host='localhost', port=6379, db=0)
 
 # Create your views here.
 def index(request):
@@ -10,6 +17,7 @@ def index(request):
         return render(request, 'films/index.html', {'movies': movies})
     elif request.method == 'POST':
         search = request.POST["search"]
+        redisClient.set('search:'+dateformat.format(datetime.datetime.now(), 'F j, Y, P'), search)
         results = []
         for movie in movies:
             if(search in movie):
